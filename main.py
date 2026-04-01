d2d96a262e183d27342eab0fed00a3ae38c748aa
Fubinii <fhw.schulz@outlook.com>
2026-04-01 11:21:13 +0200

    Fixed game loop

--- main.py
+++ main.py
@@ -9,27 +9,31 @@ default_words = 'en_US_60_SB.txt'
 words = load_words(default_words)
 game = SpellingBeerGame(words)
 
-print("SPELLING BEE(R)")
-print("------------")
-print("Current word list:", default_words, "To change, type !wordlist <filename>")
 
-while True:
-        first_input = input("Type in letters to initialize game or use !generate: ").lower()
-        if first_input.startswith("!wordlist "):
-            filename = first_input.split(" ", 1)[1]
-            try: 
-                words = load_words(filename)
-                print("Word list change successful!")
-            except FileNotFoundError:
-                print("File not found. Using default word list.")
-        elif first_input == "!generate":
-            game.from_generate()
-            break
-        else:
-            game.from_input(first_input)
-            break
+while True: # Start of the game
 
-while True:
+    print("SPELLING BEE(R)")
+    print("------------")
+    print("Current word list:", default_words, "To change, type !wordlist <filename>")
+
+    # Initialization of a round
+    while True:
+            first_input = input("Type in letters to initialize game or use !generate: ").lower()
+            if first_input.startswith("!wordlist "):
+                filename = first_input.split(" ", 1)[1]
+                try: 
+                    words = load_words(filename)
+                    print("Word list change successful!")
+                except FileNotFoundError:
+                    print("File not found. Using default word list.")
+            elif first_input == "!generate":
+                game.from_generate()
+                break
+            else:
+                game.from_input(first_input)
+                break
+            
+    # Game Loop
     game.play()
 
     print("New game? (y/n)")
