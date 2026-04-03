18d06d13fb59714bdeff478e731b8df032cff483
Fubinii <fhw.schulz@outlook.com>
2026-04-03 14:47:17 +0200

    Removed 3l words

--- dictionary/filter.py
+++ dictionary/filter.py
@@ -2,12 +2,16 @@ def filter_words(input_file, output_file):
     with open(input_file, 'r') as f:
         words = f.readlines()
     
-    filtered_words = [word for word in words if word.strip().islower() and "'" not in word]
+    filtered_words = [
+        word for word in words
+        if word.strip().islower() and "'" not in word and len(word.strip()) > 3
+    ]
     
     with open(output_file, 'w') as f:
         f.writelines(filtered_words)
 
-filter_words('en_US_60.txt', 'en_US_60_npn.txt')
+
+filter_words('en_US_60_SB.txt', 'SB_en_US.txt')
 
 
 
